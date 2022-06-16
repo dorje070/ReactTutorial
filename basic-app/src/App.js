7988e115947c910a1c1669bedcc5966f0edd1480
dorje080 <np03cs4s210254@heraldcollege.edu.np>
2022-06-16 17:25:20 +0545

    JSX Tutorial

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
 
 function App() {
-  return <h1>Hello World !</h1>;
+  function formatName(UserName) {
+    return UserName.toUpperCase();
+  }
+  function getGreeting(username) {
+    if (username) {
+      return <h1>Hello {formatName(username)}</h1>;
+    } else {
+      return <h1>Hello Stranger</h1>;
+    }
+  }
+  const name = 'Dorje Tamang';
+  const userImage = '/logo192.png';
+  const image = <img className="sample-class" src={userImage} alt="logo" />;
+  const element = (
+    <div>
+      <h1>Hello {name}</h1>
+      <p>Good to see You</p>
+      {image}
+    </div>
+  );
+  return element;
 }
 
 export default App;
