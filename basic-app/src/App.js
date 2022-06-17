9570a2c6f678ce31f193f1ae990362931fa0b50b
dorje080 <np03cs4s210254@heraldcollege.edu.np>
2022-06-17 15:23:03 +0545

    state and lifecycle and Clock example

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Welcome from './welcome';
 import Message from './Message';
+import Clock from './Clock';
 function App() {
   return (
     <div>
-      <Welcome name="John" />
+      <Clock />
     </div>
   );
 }
