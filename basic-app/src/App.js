dc9ec12dca1a659199badc9b68d0dc0c85a2354d
dorje080 <np03cs4s210254@heraldcollege.edu.np>
2022-06-17 17:08:07 +0545

    LIst and keys

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -5,11 +5,26 @@ import Clock from './Clock';
 import ToggleButton from './ToggleButton';
 import Greeting from './Greeting';
 import LoginControl from './LoginControl';
+import NumberList from './NumberList';
 
 function App() {
+  const numbers = [23, 56, 78, 90, 'Test'];
+  const numbers2 = [21, 56, 78, 91, 'Test'];
+  const todos = [
+    { id: '1', name: 'Coding', isDone: false },
+    { id: '2', name: 'Walking', isDone: true },
+  ];
   return (
     <div>
-      <LoginControl hasNewMessage={true} credit={100} hasWarning={true} />
+      <NumberList numbers={numbers} />
+      <NumberList numbers={numbers2} />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.name} - {todo.isDone ? 'Done' : ''}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ basic-app/src/NumberList.js
@@ -0,0 +1,10 @@
+function NumberList(props) {
+  return (
+    <ul>
+      {props.numbers.map((number) => (
+        <li key={number}>{number}</li>
+      ))}
+    </ul>
+  );
+}
+export default NumberList;
