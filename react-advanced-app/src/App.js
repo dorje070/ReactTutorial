2f851f6af82b8a3ae813d9891913d39fae813673
dorje080 <np03cs4s210254@heraldcollege.edu.np>
2022-06-21 15:42:21 +0545

    Use Reducer hook

--- react-advanced-app/src/App.js
+++ react-advanced-app/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Counter from './Counter';
+import Layout from './Layout';
+import Maincontent from './Maincontent';
 import NewCounter from './NewCounter';
 import NewUserinfo from './NewUserinfo';
 import Userinfo from './Userinfo';
+import UserList from './UserList';
 
 function App() {
   return (
     <div>
       {/* <Userinfo /> */}
-      <NewUserinfo />
+      {/* <NewUserinfo /> */}
+      {/* <Layout>
+        <Maincontent />
+      </Layout> */}
+      <UserList />
     </div>
   );
 }
